ea8c9b22d560796295abaded49ba8290110f209e
jmoralesss <mail@jmorales.net>
2024-01-17 19:59:16 -0600

    updated metadata

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "jwc")]
+#[command(author = "Jonathan Morales")]
+#[command(version = "1.0")]
+#[command(about = "Print newline, word, and byte counts for each FILE, and a total line if more than one FILE is specified. With no FILE, or when FILE is -, r", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    two: String,
+    #[arg(long)]
+    one: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("two: {:?}", cli.two);
+    println!("one: {:?}", cli.one);
 }
